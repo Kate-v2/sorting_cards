1b6449141dd5193be89c69e2bfa93d7b778997cb
Katelyn <katelyn.rimkunas@gmail.com>
2018-08-18 13:14:43 -0600

    Add correctness functionality for guess

--- lib/guess.rb
+++ lib/guess.rb
@@ -1,6 +1,8 @@
 # Create a class for the Guess functionality
 
-require "pry"
+require 'pry'
+
+require './lib/card'
 
 
 class Guess
@@ -10,4 +12,33 @@ class Guess
     @response = response
     @card = card
   end
+
+  def convert_card_value_to_sentence
+    card.value + " of " + card.suit
+  end
+
+  def correct?
+    valid = false
+    if response == convert_card_value_to_sentence
+      valid = true
+    end
+    return valid
+  end
+
+  def feedback
+    answer = ""
+    # correctness = correct?
+    if correct? == true
+      p answer = "Correct!"
+    else
+      p answer = "Incorrect."
+    end
+    return answer
+  end
+
+
 end
+
+# card = Card.new("10", "Hearts")
+# guess = Guess.new("10 of Hearts", card)
+# binding.pry
--- test/guess_test.rb
+++ test/guess_test.rb
@@ -1,4 +1,4 @@
-require "pry"
+require 'pry'
 
 require 'minitest/autorun'
 require 'minitest/pride'
@@ -11,5 +11,32 @@ class GuessTest < Minitest::Test
     assert_instance_of Guess, actual
   end
 
+  def test_it_gets_instance_variables
+    card = Card.new("10", "Hearts")
+    guess = Guess.new("10 of Hearts", card)
+    assert_equal card, guess.card
+    assert_equal "10 of Hearts", guess.response
+  end
+
+  def test_it_can_convert_card_value_to_sentence
+    card = Card.new("10", "Hearts")
+    guess = Guess.new("10 of Hearts", card)
+    assert_equal "10 of Hearts", guess.convert_card_value_to_sentence
+  end
+
+  def text_it_can_compare_real_and_guessed_correctness
+    card = Card.new("10", "Hearts")
+    guess = Guess.new("10 of Hearts", card)
+    assert_equal true, guess.correct?
+  end
+
+  def test_it_can_print_feedback_about_response
+    card = Card.new("10", "Hearts")
+    guess_1 = Guess.new("10 of Hearts", card)
+    assert_equal "Correct!", guess_1.feedback
+
+    guess_2 = Guess.new("0 of Hearts", card)
+    assert_equal "Incorrect.", guess_2.feedback
+  end
 
 end
